8db99b67484dca9e4438f7c9de90f449ed695309
johndanielzz <jd0447754@gmail.com>
2026-02-08 09:48:33 -0800

    Create main React component src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to MATRIXAI!</h1>
+            {/* Your main component code goes here */}
+        </div>
+    );
+}
+
+export default App;
